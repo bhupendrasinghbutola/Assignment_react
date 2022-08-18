25b8d7ff102b20dff5a295dde5bf488bb9a3eebf
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-08-18 11:39:13 +0530

    added work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {HashRouter,Route,Routes} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home  from './pages/Home';
+
+// import App from './App';
+const App =()=>{
+  return(
+    <HashRouter>
+      
+        <Routes>
+        
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </HashRouter>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +29,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
